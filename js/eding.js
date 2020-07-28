10133d6fc164c16a0a7d2f8199403e61df6991e7
wonjunkang <kwjcomein@gmail.com>
2020-07-28 16:58:02 +0900

    ending game finsh

--- js/eding.js
+++ js/eding.js
@@ -2,22 +2,25 @@
 // const tag list
 const wordView = document.querySelector(".main__text-outPut > span");
 const chat = document.getElementById("chatWord");
-const wordViewText = wordView.textContent;
-const chatText = chat.value;
+const chatBtn = document.querySelector(".main__text-btn");
+const resultText = document.querySelector('.result');
+const endingForm = document.getElementById("endingForm");
+const timeBar = document.getElementById("timeBar");
+wordView.textContent = "제로초";
 
-// input word view
-function inputWordView() {
-  chat.addEventListener("keydown", (key) => {
-    if (key.keyCode === 13 && wordViewText === "") {
-      wordView.innerHTML = "";
-      let word = chat.value;
-      wordView.innerHTML = word;
-      chat.value = "";
+
+
+
+endingForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    if (wordView.textContent[wordView.textContent.length-1] === chat.value[0]){
+        resultText.textContent = "딩동댕";
+        wordView.textContent =  chat.value;
+        chat.value = "";
+        chat.focus();
+    } else {
+        resultText.textContent = "땡";
+        chat.value = "";
+        chat.focus();
     }
-  });
-}
-inputWordView();
-// input word check
-function wordViewCheck() {}
-wordViewCheck();
-// input word timeout
+});
