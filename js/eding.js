172981f6cf934d933a70ed2acb312fd426d94e12
wonjunkang <kwjcomein@gmail.com>
2020-07-29 13:00:26 +0900

    JS: modified

--- js/eding.js
+++ js/eding.js
@@ -3,30 +3,27 @@
 const wordView = document.querySelector(".main__text-outPut > span");
 const chat = document.getElementById("chatWord");
 const chatBtn = document.querySelector(".main__text-btn");
-const resultText = document.querySelector('.result');
+const resultText = document.querySelector(".result");
 const endingForm = document.getElementById("endingForm");
 const timeBar = document.getElementById("timeBar");
 wordView.textContent = "제로초";
 
-// setInterval(() => {
-//     timeBar.value = timeBar.value - 1;
-//     if (timeBar.value === 0){
-//         alert("d");
-        
-//     }
-// }, 1000);
+setInterval(() => {
+  timeBar.value = timeBar.value - 1;
+  if (timeBar.value === 0) {
+  }
+}, 1000);
 
-
-endingForm.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    if (wordView.textContent[wordView.textContent.length-1] === chat.value[0]){
-        resultText.textContent = "딩동댕";
-        wordView.textContent =  chat.value;
-        chat.value = "";
-        chat.focus();
-    } else {
-        resultText.textContent = "땡";
-        chat.value = "";
-        chat.focus();
-    }
+endingForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (wordView.textContent[wordView.textContent.length - 1] === chat.value[0]) {
+    resultText.textContent = "딩동댕";
+    wordView.textContent = chat.value;
+    chat.value = "";
+    chat.focus();
+  } else {
+    resultText.textContent = "땡";
+    chat.value = "";
+    chat.focus();
+  }
 });
